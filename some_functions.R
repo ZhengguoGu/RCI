b29fd04a5421550bb9898419b5de6e91563eeffb
Zhengguo MacbookPro <zhengguo.gu@gmail.com>
2018-11-29 11:54:03 +0100

    update

--- some_functions.R
+++ some_functions.R
@@ -1,6 +1,6 @@
 ######### some functions used in the simulation #############
 
-
+# 1. GRM
 GRM_func <- function(abil, itempar){
   
   numeritor <- exp(sweep((abil - itempar[, -1]), 1, itempar[, 1], "*"))
@@ -12,3 +12,32 @@ GRM_func <- function(abil, itempar){
 }
 
 
+# 2. p-value
+pvalue <- function(z_score, two_tail){
+  
+  if(missing(two_tail)){
+    two_tail <- 2  #i.e., 2 tails
+  }
+  
+  if(two_tail == 2){
+    if(z_score < 0){
+      p_value <- 2 * stats::pnorm(z_score)
+    }else if (z_score > 0){
+      p_value <- 2 * (1 - stats::pnorm(z_score))
+    }else{
+      p_value <- 0
+    }
+  }else if (two_tail == 1){
+    if(z_score < 0){
+      p_value <- stats::pnorm(z_score)
+    }else if (z_score > 0){
+      p_value <- 1 - stats::pnorm(z_score)
+    }else{
+      p_value <- 0
+    }
+  }else{
+    print("two_tail allows for 2 values only: 1: one-tail test, and 2: two-tail test")
+  }
+    
+  return(p_value)
+}
