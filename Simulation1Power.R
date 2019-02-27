f78683770b473c075c06a4d3321068dc332af1b7
Zhengguo MacbookPro <zhengguo.gu@gmail.com>
2019-02-27 19:09:25 +0100

    update

--- Simulation1Power.R
+++ Simulation1Power.R
@@ -25,12 +25,11 @@ change_theta = c(0.5, 1)
 condition <- expand.grid(test_length, item_character, change_theta,  CO_effect)
 colnames(condition) <- c("test_length", "item_character", "magnitude_change", "carry-over")
 
-
 Final_result <- list()
 num_test <- 1
 while(num_test <= dim(condition)[1]){
   
-  if (condition[num_test, 2] == "parallel") {
+  if (condition[num_test, 2] == "parallel") {#identical item parameters
     
     itempar <- matrix(NA,condition[num_test, 1],5)
     itempar[,1] <- runif(1,1.5,2.5)   # discrimination
@@ -40,7 +39,7 @@ while(num_test <= dim(condition)[1]){
     itempar[,4] <- avg_beta + .25
     itempar[,5] <- avg_beta + .75
     
-  } else {
+  } else { #non-identical item parameters
     
     itempar <- matrix(NA,condition[num_test, 1],5)
     itempar[,1] <- runif(condition[num_test, 1],1.5,2.5)  # discrimination
@@ -64,14 +63,14 @@ while(num_test <= dim(condition)[1]){
     NoCarry_index <- sample(1000, floor(1000 * (1-0.5)), replace = FALSE)  #here we fix the persons who do not show carryover
   }
   
-  cl <- makeCluster(2)
+  cl <- makeCluster(6)
   registerDoSNOW(cl)
   sim_result <- foreach(i = 1:100) %dorng% {
     
     pretest <- t(sapply(theta_pre, FUN = GRM_func,  itempar = itempar))
     posttest <- t(sapply(theta_post, FUN = GRM_func,  itempar = itempar))  
     
-    if(condition[num_test, 4] != "non"){  
+    if(condition[num_test, 4] != "non"){    #i.e., there is carryover effect
       posttest <- carry_over(pretest, posttest, NoCarry_index)
     }
     
@@ -111,7 +110,7 @@ while(num_test <= dim(condition)[1]){
   }
   stopCluster(cl)
   
-  Powers <- Reduce('+', sim_result) / 100  # parallel-generated 100 matrices, and we add these matrices together, and then compute the empirical Type 1 error rate 
+  Powers <- Reduce('+', sim_result) / 100  # parallel-generated 100 matrices, and we add these matrices together
   result <- cbind(theta_pre, Powers)
   colnames(result) <- c("theta", "eq0", "eq1", "eq2", "eq3")
   Final_result[[num_test]] <- result
@@ -119,11 +118,102 @@ while(num_test <= dim(condition)[1]){
   num_test = num_test + 1
 }
 
+################## summarizing results ############
+library(ggplot2)
+library(gridExtra)
+library(grid)
+library(tidyr)
+pic_function <- function(data){
+  longdata<- gather(data.frame(data), equation, result, eq0:eq3, factor_key=TRUE)
+  p <- ggplot(longdata, aes(x = longdata$theta, y = longdata$result, colour = longdata$equation)) + 
+    geom_line(aes(group = longdata$equation))  +
+    scale_y_continuous(breaks = c(0, .5, 1), limits = c(0,1)) +
+    xlim(-3, 3) + 
+    theme(legend.position = "none", axis.text=element_text(size=8), 
+          axis.title.x = element_text(size=10), axis.title.y = element_text(size=10)) +
+    labs(x=expression(theta[pre]), y="Power") +
+    facet_grid(equation ~ .)
+  return(p)
+} #!!! xlim and ylim are truncated, therefore when generating plots, we see warning messages.
 
-plot(Final_result[[30]][,2], ylim = c(0, 1), type = "l")
-lines(Final_result[[30]][,3], col = "red")
-lines(Final_result[[30]][,4], col = "green")
-lines(Final_result[[30]][,5], col = "purple")
 
-plot(Final_result[[1]][,2], ylim = c(0, 1), type = "l")
-lines(Final_result[[1]][,3], col = "red")
+
+### 1. test length against carryover effect, when identical items and change = .5 ####
+condition[27, ]
+
+p1 <- pic_function(data.frame(Final_result[[1]]))   #test length = 5, parallel, magnitude_change = .5, no carryover
+p2 <- pic_function(data.frame(Final_result[[13]]))  #test length = 5, parallel, magnitude_change = .5, 30% carryover
+p3 <- pic_function(data.frame(Final_result[[25]]))  #test length = 5, parallel, magnitude_change = .5, 50% carryover
+p4 <- pic_function(data.frame(Final_result[[2]]))  #test length = 15, parallel, magnitude_change = .5, no carryover
+p5 <- pic_function(data.frame(Final_result[[14]]))  #test length = 15, parallel, magnitude_change = .5, 30% carryover
+p6 <- pic_function(data.frame(Final_result[[26]])) #test length = 15, parallel, magnitude_change = .5, 50% carryover
+p7 <- pic_function(data.frame(Final_result[[3]]))  #test length = 40, parallel, magnitude_change = .5, no carryover
+p8 <- pic_function(data.frame(Final_result[[15]]))  #test length = 40, parallel, magnitude_change = .5, 30% carryover
+p9 <- pic_function(data.frame(Final_result[[27]])) #test length = 40, parallel, magnitude_change = .5, 50% carryover
+
+grid.arrange(
+  p1, p2, p3,
+  p4, p5, p6,
+  p7, p8, p9,
+  nrow = 3
+) #warnings are due to the truncation of xlim
+
+### 2. test length against carryover effect, when non-identical items and change = .5 ####
+condition[30, ]
+
+p1 <- pic_function(data.frame(Final_result[[4]]))   #test length = 5, non-parallel, magnitude_change = .5, no carryover
+p2 <- pic_function(data.frame(Final_result[[16]]))  #test length = 5, non-parallel, magnitude_change = .5, 30% carryover
+p3 <- pic_function(data.frame(Final_result[[28]]))  #test length = 5, non-parallel, magnitude_change = .5, 50% carryover
+p4 <- pic_function(data.frame(Final_result[[5]]))  #test length = 15, non-parallel, magnitude_change = .5, no carryover
+p5 <- pic_function(data.frame(Final_result[[17]]))  #test length = 15, non-parallel, magnitude_change = .5, 30% carryover
+p6 <- pic_function(data.frame(Final_result[[29]])) #test length = 15, non-parallel, magnitude_change = .5, 50% carryover
+p7 <- pic_function(data.frame(Final_result[[6]]))  #test length = 40, non-parallel, magnitude_change = .5, no carryover
+p8 <- pic_function(data.frame(Final_result[[18]]))  #test length = 40, non-parallel, magnitude_change = .5, 30% carryover
+p9 <- pic_function(data.frame(Final_result[[30]])) #test length = 40, non-parallel, magnitude_change = .5, 50% carryover
+
+grid.arrange(
+  p1, p2, p3,
+  p4, p5, p6,
+  p7, p8, p9,
+  nrow = 3
+) #warnings are due to the truncation of xlim
+
+### 3. test length against carryover effect, when identical items and change = 1 ####
+condition[33, ]
+
+p1 <- pic_function(data.frame(Final_result[[7]]))   #test length = 5, parallel, magnitude_change = 1, no carryover
+p2 <- pic_function(data.frame(Final_result[[19]]))  #test length = 5, parallel, magnitude_change = 1, 30% carryover
+p3 <- pic_function(data.frame(Final_result[[31]]))  #test length = 5, parallel, magnitude_change = 1, 50% carryover
+p4 <- pic_function(data.frame(Final_result[[8]]))  #test length = 15, parallel, magnitude_change = 1, no carryover
+p5 <- pic_function(data.frame(Final_result[[20]]))  #test length = 15, parallel, magnitude_change = 1, 30% carryover
+p6 <- pic_function(data.frame(Final_result[[32]])) #test length = 15, parallel, magnitude_change = 1, 50% carryover
+p7 <- pic_function(data.frame(Final_result[[9]]))  #test length = 40, parallel, magnitude_change = 1, no carryover
+p8 <- pic_function(data.frame(Final_result[[21]]))  #test length = 40, parallel, magnitude_change = 1, 30% carryover
+p9 <- pic_function(data.frame(Final_result[[33]])) #test length = 40, parallel, magnitude_change = 1, 50% carryover
+
+grid.arrange(
+  p1, p2, p3,
+  p4, p5, p6,
+  p7, p8, p9,
+  nrow = 3
+) #warnings are due to the truncation of xlim
+
+### 4. test length against carryover effect, when non-identical items and change = 1 ####
+condition[36, ]
+
+p1 <- pic_function(data.frame(Final_result[[10]]))   #test length = 5, non-parallel, magnitude_change = 1, no carryover
+p2 <- pic_function(data.frame(Final_result[[22]]))  #test length = 5, non-parallel, magnitude_change = 1, 30% carryover
+p3 <- pic_function(data.frame(Final_result[[34]]))  #test length = 5, non-parallel, magnitude_change = 1, 50% carryover
+p4 <- pic_function(data.frame(Final_result[[11]]))  #test length = 15, non-parallel, magnitude_change = 1, no carryover
+p5 <- pic_function(data.frame(Final_result[[23]]))  #test length = 15, non-parallel, magnitude_change = 1, 30% carryover
+p6 <- pic_function(data.frame(Final_result[[35]])) #test length = 15, non-parallel, magnitude_change = 1, 50% carryover
+p7 <- pic_function(data.frame(Final_result[[12]]))  #test length = 40, non-parallel, magnitude_change = 1, no carryover
+p8 <- pic_function(data.frame(Final_result[[24]]))  #test length = 40, non-parallel, magnitude_change = 1, 30% carryover
+p9 <- pic_function(data.frame(Final_result[[36]])) #test length = 40, non-parallel, magnitude_change = 1, 50% carryover
+
+grid.arrange(
+  p1, p2, p3,
+  p4, p5, p6,
+  p7, p8, p9,
+  nrow = 3
+) #warnings are due to the truncation of xlim
